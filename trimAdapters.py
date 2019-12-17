2363be582faca15a47d9f53f29c66271d3b22c54
narendrameena <narendrabhagri03@gmail.com>
2019-12-17 21:25:54 +0000

    trim all adapters

--- trimAdapters.py
+++ trimAdapters.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 '''
 @author: narumeena
-@description: doing quality check of all RNA samples
+@description: comprehensive quality profiling for both before and after filtering data (quality curves, base contents, KMER, Q20/Q30, 
+GC Ratio, duplication, adapter contents...) filter out bad reads (too low quality, too short, or too many N...) cut low quality bases
+for per read in its 5' and 3' by evaluating the mean quality from a sliding window (like Trimmomatic but faster). trim all reads in 
+front and tail cut adapters. Adapter sequences can be automatically detected, which means you don't have to input the adapter sequences
+to trim them. correct mismatched base pairs in overlapped regions of paired end reads, if one base is with high quality while the other 
+is with ultra low quality trim polyG in 3' ends, which is commonly seen in NovaSeq/NextSeq data. Trim polyX in 3' ends to remove unwanted 
+polyX tailing (i.e. polyA tailing for mRNA-Seq data) preprocess unique molecular identifier (UMI) enabled data, shift UMI to sequence name.
+report JSON format result for further interpreting.
+
 @study IPS cell lines 
 '''
 
@@ -12,9 +20,8 @@ import errno,subprocess
 main variables 
 '''
 inputRnaDirName     = '/mnt/hdd1/narendra/cambridge/projects/inProgress/iPSCLinesProteogenomics/data/RNA/'
-outputDirName       = '/mnt/hdd1/narendra/cambridge/projects/inProgress/iPSCLinesProteogenomics/analysis/qualityControl/RNA/PRJEB7388/' 
-
-
+outputDirName       = '/mnt/hdd1/narendra/cambridge/projects/inProgress/iPSCLinesProteogenomics/analysis/trimAdapters/RNA/PRJEB7388/' 
+inputForCommand     = '/mnt/hdd1/narendra/cambridge/projects/inProgress/iPSCLinesProteogenomics/data/RNA/PRJEB7388/'
 
 def getListOfFiles(dirName):
     '''
@@ -49,7 +56,8 @@ def mkdir_p(path):
         else:
             raise
 
-def runFastQc(fastq_path, output_dir, options=['--extract -t 10',]):
+
+def runFastP(fastq_path, output_dir, file_name,  options=[' ',]):
     """
     Run the fastqc program on a specified fastq file and return the output directory path.
 
@@ -59,6 +67,8 @@ def runFastQc(fastq_path, output_dir, options=['--extract -t 10',]):
         Path to the fastq file to analyze.
     output_dir : str
         Directory where output will be written. It will be created if it does not exist.
+    filename : str
+        prefix of fastq file to analyze
     options : list of str (optional)
         Command-line flags and options to fastqc. Default is --extract.
 
@@ -68,9 +78,9 @@ def runFastQc(fastq_path, output_dir, options=['--extract -t 10',]):
         The path to the directory containing the detailed output for this fastq file.
         It will be a subdirectory of the specified output dir.
     """
-    mkdir_p(output_dir)
+    mkdir_p(output_dir + file_name)
 
-    command = "fastqc {} -o {} {}".format(' '.join(options), output_dir, fastq_path)
+    command = "fastp --unpaired1 --unpaired2 -w 20  -i "+fastq_path+file_name+"/"+file_name+ "_1.fastq.gz -o "+output_dir+file_name+"/"+file_name+"_trim_1.fastq.gz -I  "+fastq_path+file_name+"/"+file_name+"_2.fastq.gz -O "+output_dir+file_name+"/"+file_name+"_trim_2.fastq.gz".format(' '.join(options))
     subprocess.check_call(command, shell=True)
 
     # Fastqc creates a directory derived from the basename
@@ -98,8 +108,8 @@ def main():
     listOfFiles = [x for x in listOfFiles if 'fastq.gz' in x]
 
     #running fastqc 
-    [runFastQc(x, outputDirName + os.path.split(os.path.dirname(x))[1]) for x in listOfFiles]    
-
+    [runFastP(inputForCommand, outputDirName , os.path.split(os.path.dirname(x))[1]) for x in listOfFiles]    
+    #print(list(dict.fromkeys([os.path.split(os.path.dirname(x))[1] for x in listOfFiles])))
 
 if __name__=="__main__":
     main()
